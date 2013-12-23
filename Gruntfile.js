bc82d651e6597665337d1ce4d58fdaf2d724efb9
Timm Stokke <timm@stokke.me>
2013-12-23 17:09:55 +0100

    Grunt now takes care of (almost) whole build process.
    @todo Fix & verify that clean bower install will build as expected (currently font awesome's .css is renamed to .less locally, etc)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,102 @@
+/*
+ * Based on http://24ways.org/2013/grunt-is-not-weird-and-hard/
+ */
+
 module.exports = function(grunt) {
 
-	// 1. All configuration goes here
+	// CONFIGURATION
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
+		// Copy:
+		copy: {
+			main: {
+				files: [
+					{expand: true, src: ['bower_components/font-awesome/fonts/*'], dest: 'fonts/', filter: 'isFile'},
+				]
+			}
+		},
+		// Combine:
 		concat: {
 			dist: {
 				src: [
-					'bower_components/jquery/jquery.min.js', // jQuery
-					'js/*.js'  // All local JS files
+					'bower_components/jquery/jquery.min.js',
+					'bower_components/owlcarousel/owl-carousel/owl.carousel.min.js',
+					'js/main.js'  // All local JS files
 				],
-				dest: 'js/concat.min.js',
+				dest: 'js/build/main.concat.js',
 			}
-		}
-
+		},
+		// Minimize JS
+		uglify: {
+			build: {
+				src: 'js/build/main.concat.js',
+				dest: 'js/main.min.js'
+			}
+		},
+		// Optimize images
+		imagemin: {
+			dynamic: {
+				files: [{
+					expand: true,
+					cwd: 'img/',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'img/'
+				}]
+			}
+		},
+		// Compile CSS
+		less: {
+			development: {
+				options: {
+					paths: ['css'],
+					sourceMap : true
+				},
+				files: {
+					'css/main.css': 'css/main.less'
+				}
+			},
+			production: {
+				options: {
+					paths: ['css'],
+					cleancss : true
+				},
+				files: {
+					'css/main.min.css': 'css/main.less'
+				}
+			}
+		},
+		// Watch
+		watch: {
+			options: {
+				livereload: true,
+			},
+			scripts: {
+				files: ['js/*.js'],
+				tasks: ['concat', 'uglify'],
+				options: {
+					spawn: false,
+				},
+			},
+			css: {
+				files: ['css/*.less', 'css/assets/*.less'],
+				tasks: ['less'],
+				options: {
+					spawn: false,
+				}
+			}
+		},
 	});
 
-	// 3. Where we tell Grunt we plan to use this plug-in.
+	// PLUGINS
 	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	// 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-	grunt.registerTask('default', ['concat']);
+	grunt.registerTask('default', ['copy', 'concat', 'uglify', 'imagemin', 'less', 'watch']);
 
 };
